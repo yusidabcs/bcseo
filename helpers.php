c2e4efaba0994e26279cde32b8094016d8ee27b3
yusidabcs <agusyusida@gmail.com>
2017-06-23 11:15:28 +0800

    update seo

--- helpers.php
+++ helpers.php
@@ -2,9 +2,12 @@
 
 function bcs_seo()
 {
+
 	$html = SEO::generate(true);
     
-    $html = $html.'<script type="application/ld+json">
+    if(Route::currentRouteName() == 'homepage')
+    {
+    	$html = $html.'<script type="application/ld+json">
     {
       "@context": "http://schema.org",
       "@type": "WebSite",
@@ -24,7 +27,9 @@ function bcs_seo()
         "name":"'. setting('core::site-name') .'",
         "logo":"'. setting('core::logo') .'"
     }
-    </script>';
+    </script>';	
+    }
+    
 
     return preg_replace( "/\r|\n/", "", $html );
 }
